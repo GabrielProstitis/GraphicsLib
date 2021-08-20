022cce1f450b217ad67d46ca15f532096bd40649
Prosti19 <gabriprostitis@gmail.com>
2021-08-20 22:26:25 +0200

    Added src folder and CMakeLists.txt files

---
+++ build/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test.dir/src/main.cpp.o"
+  "CMakeFiles/Test.dir/src/main.cpp.o.d"
+  "Test"
+  "Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hey :)" << std::endl;
+    return 0;
+}
