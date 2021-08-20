826bffec8c8f4a45bb34835ef7f9b1c3898f6777
Prosti19 <gabriprostitis@gmail.com>
2021-08-20 22:52:09 +0200

    Added GLFW and GLEW

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,70 @@
-#include <iostream>
+#include <glew.h>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(void)
 {
-    std::cout << "Hey :)" << std::endl;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK)
+        return -1;
+    
+    GLfloat data[] = 
+    {
+        -0.5f, -0.5f,
+         0.5f,  -0.5f,
+         0.0f,  0.5f
+    };
+
+    GLuint vb;
+    glGenBuffers(1, &vb);
+    glBindBuffer(GL_ARRAY_BUFFER, vb);
+    glBufferData(GL_ARRAY_BUFFER, 2 * 3 * sizeof(GLfloat), data, GL_STATIC_DRAW);
+
+    GLuint va;
+    glBindVertexArray(va);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
+    
+    GLuint indicies[] = 
+    {
+        0, 1, 2
+    };
+
+    GLuint ib;
+    glGenBuffers(1, &ib);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ib);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(GLuint), indicies, GL_STATIC_DRAW);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
