4f6e6ae0edf24a22d8b34a1f63b71b3409e9243a
iron <swatlcpd@gmail.com>
2021-08-22 02:34:29 +0300

    Renderer Created

--- src/Graphics/Renderer.hpp
+++ src/Graphics/Renderer.hpp
@@ -17,13 +17,13 @@ public:
 	Renderer(glm::mat4 view) : view(view){ }*/
 public:
 	Renderer() {}
-	void RenderMesh(const Mesh& mesh)
+	void render(const Mesh& mesh)
 	{
 		glBindVertexArray(mesh.m_Vao);
 		glBindBuffer(GL_ARRAY_BUFFER, mesh.m_Vao);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.m_Ibo);
 
-		glDrawElements(GL_TRIANGLES, 222, GL_UNSIGNED_INT, nullptr);
+		glDrawElements(GL_TRIANGLES, mesh.m_Indices.size() * sizeof(uint32_t), GL_UNSIGNED_INT, 0);
 	}
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,46 @@
 #include "GraphicsLib.hpp"
-
+#include "Graphics/Renderer.hpp"
 int main(void)
 {
 
     Window window(640, 480, "Graphics Lib Test");
     InitGraphics(window);
 
-    float data[] =
+    std::vector<float> data =
     {
         -0.5f, -0.5f,
         0.5f, -0.5f,
-        0.0f, 0.5f };
+        0.0f, 0.5f 
+    };
 
     uint32_t vb;
     glGenBuffers(1, &vb);
     glBindBuffer(GL_ARRAY_BUFFER, vb);
-    glBufferData(GL_ARRAY_BUFFER, 2 * 3 * sizeof(float), data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 2 * 3 * sizeof(float), &data[0], GL_STATIC_DRAW);
 
     uint32_t va = 0;
     glBindVertexArray(va);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-    uint32_t indicies[] =
-    {
-        0, 1, 2 };
+    std::vector<uint32_t> indices =
+    { 0, 1, 2 };
 
     uint32_t ib;
     glGenBuffers(1, &ib);
+    std::cout << (ib);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ib);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(uint32_t), indicies, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(uint32_t), &indices[0], GL_STATIC_DRAW);
 
-    Mesh triangle(data, indicies, 3, vb, va, ib);
+    Mesh Triangle(data, indices, va, vb, ib);
 
-    /* Loop until the user closes the window */
+    Renderer MainRenderer;
     while (!glfwWindowShouldClose(window.GetWindow()))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, triangle.m_Count, GL_UNSIGNED_INT, 0);
+        MainRenderer.render(Triangle);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window.GetWindow());
